213082257698bfd0ea785df3aa75f41dfdb35727
Priyanka <pbhaukajee@gmail.com>
2023-12-13 13:44:11 -0600

    add code for size of grid

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const container = document.querySelector("#container");
+const size = document.querySelector(".size");
+const black = document.querySelector(".black");
+const random = document.querySelector(".random");
+const erase = document.querySelector(".erase");
+const reset = document.querySelector(".reset");
+
 let content;
 let grid;
-let size;
 
-// do {
-//   size = prompt("size (1 to 100): ");
-// } while (size < 0 || size > 100);
-
-for (let i = 0; i < size; i++) {
+//Create grids
+for (let i = 0; i < 16; i++) {
   content = document.createElement("div");
   content.classList.add("content");
   container.appendChild(content);
-  for (let j = 0; j < size; j++) {
+  for (let j = 0; j < 16; j++) {
     grid = document.createElement("div");
     grid.classList.add("grid");
     content.appendChild(grid);
@@ -20,3 +22,11 @@ for (let i = 0; i < size; i++) {
     });
   }
 }
+
+//Ask for size and create grid per size
+size.addEventListener("click", () => {
+  s = prompt("Input size (1 to 100): ");
+  if (s <= 0 || s > 100) {
+    s = prompt("Input correct size (1 to 100): ");
+  }
+});
