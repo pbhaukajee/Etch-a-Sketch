6ad96411aa6db4319ee49eec406985a62319b675
Priyanka <pbhaukajee@gmail.com>
2023-12-13 11:10:34 -0600

    prompt for size

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector("#container");
 let content;
 let grid;
-for (let i = 0; i < 16; i++) {
+
+const size = prompt("size: ");
+for (let i = 0; i < size; i++) {
   content = document.createElement("div");
   content.classList.add("content");
   container.appendChild(content);
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < size; j++) {
     grid = document.createElement("div");
     grid.classList.add("grid");
     content.appendChild(grid);
